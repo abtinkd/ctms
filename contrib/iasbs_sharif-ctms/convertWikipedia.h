22a32527965af76fcb7b12d0384ea7d16e2deb03
Abt@Win <abtinkh+github@gmail.com>
2019-01-11 15:04:16 -0800

    clean directory

--- contrib/iasbs_sharif-ctms/convertWikipedia.h
+++
@@ -1,4 +0,0 @@
-#ifndef CONVERT_WIKIPEDIA_H
-#define CONVERT_WIKIPEDIA_H
-int convertWiki(char fileLoc[], bool includeNeutralVotes = false);
-#endif // !CONVERT_WIKIPEDIA_H
--- contrib/iasbs_sharif-ctms/mysignnet.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_SIGNED_NETWORK_H
-#define MY_SIGNED_NETWORK_H
-
-#include "signnet.h"
-#include "Snap.h"
-
-#endif // !MY_SIGNED_NETWORK_H
