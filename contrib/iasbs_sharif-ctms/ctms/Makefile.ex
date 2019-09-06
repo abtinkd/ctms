9ad9f2d6abd974f9898681a4e9d0ce3fa72a4b32
Abt@Win <abtinkh+github@gmail.com>
2019-09-06 16:03:29 -0700

    working MakeFiles

---
+++ contrib/iasbs_sharif-ctms/ctms/Makefile.ex
@@ -0,0 +1,9 @@
+#
+#	configuration variables for the example
+
+## Main application file
+MAIN = ctmsmain
+CXXFLAGS = -std=c++11 -Wall
+DEPH = ../$(EXSNAPEXP)/signnet.h ctmsnet.h sign_prediction.h ml.h convert_wikipedia.h
+DEPCPP = ../$(EXSNAPEXP)/signnet.cpp ctmsnet.cpp sign_prediction.cpp ml.cpp convert_wikipedia.cpp
+
