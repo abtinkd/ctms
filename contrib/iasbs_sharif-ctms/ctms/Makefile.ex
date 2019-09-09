20ace184afb4d69c9261b052bc4934e512324296
Abtin K <abtinkh+github@gmail.com>
2019-09-09 12:29:49 -0700

    Update Makefile.ex

--- contrib/iasbs_sharif-ctms/ctms/Makefile.ex
+++ contrib/iasbs_sharif-ctms/ctms/Makefile.ex
@@ -3,7 +3,7 @@
 
 ## Main application file
 MAIN = ctmsmain
-CXXFLAGS = -std=c++11 -Wall
+CXXFLAGS += -std=c++11
 DEPH = ../$(EXSNAPEXP)/signnet.h ctmsnet.h sign_prediction.h ml.h convert_wikipedia.h
 DEPCPP = ../$(EXSNAPEXP)/signnet.cpp ctmsnet.cpp sign_prediction.cpp ml.cpp convert_wikipedia.cpp
 
