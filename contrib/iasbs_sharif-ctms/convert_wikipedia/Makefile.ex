0dbd68050984e67d9e8d835e8c9e55433c15f602
Abtin K <abtinkh+github@gmail.com>
2019-09-11 15:57:19 -0700

    Update Makefile.ex

--- contrib/iasbs_sharif-ctms/convert_wikipedia/Makefile.ex
+++ contrib/iasbs_sharif-ctms/convert_wikipedia/Makefile.ex
@@ -2,7 +2,7 @@
 #	configuration variables for the example
 
 ## Main application file
-MAIN = covertmain
+MAIN = convertmain
 CXXFLAGS += -std=c++11
 DEPH = convert_wikipedia.h
 DEPCPP = convert_wikipedia.cpp
