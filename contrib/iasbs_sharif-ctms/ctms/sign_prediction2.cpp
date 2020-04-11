8ba2f777a7e3c5fd63f09d4c32e23eacc692206e
Abt@Win <abtinkh+github@gmail.com>
2020-04-11 23:35:07 +0430

    sign predictor classes template

---
+++ contrib/iasbs_sharif-ctms/ctms/sign_prediction2.cpp
@@ -0,0 +1,49 @@
+#include "sign_prediction2.h"
+
+void TNaivePredictor::build() {
+
+}
+
+void TBalanceBasedPredictor::build() {
+
+}
+
+void TStatusBasedPredictor::build() {
+
+}
+
+void::TLogisticRegression::extractFeatures() {
+
+}
+
+void TLogisticRegression::build() {
+
+}
+
+void TCTMSProbabilisticInference::extractFeatures() {
+
+}
+
+void TCTMSProbabilisticInference::build() {
+
+}
+
+int TNaivePredictor::predict(const TInt srcNId, const TInt desNId) {
+	return 1;
+}
+
+int TBalanceBasedPredictor::predict(const TInt srcNId, const TInt desNId) {
+	return 1;
+}
+
+int TStatusBasedPredictor::predict(const TInt srcNId, const TInt desNId) {
+	return 1;
+}
+
+int TLogisticRegression::predict(const TInt srcNId, const TInt desNId) {
+	return 1;
+}
+
+int TCTMSProbabilisticInference::predict(const TInt srcNId, const TInt desNId) {
+	return 1;
+}
---
+++ contrib/iasbs_sharif-ctms/ctms/sign_prediction2.h
@@ -0,0 +1,57 @@
+#ifndef SIGN_PREDICTION2_H
+#define SIGN_PREDICTION2_H
+
+#include "ctmsnet.h"
+#include <Snap.h>
+#include "ml.h"
+
+class TSignPredictor {
+protected:	
+	PCtmsNet network;	
+public:
+	TSignPredictor(const PCtmsNet& net) : network(net) {}
+	virtual void build() = 0;
+	virtual int predict(const TInt srcNId, const TInt desNId) = 0;
+};
+
+class TNaivePredictor : public TSignPredictor {
+public:
+	TNaivePredictor(const PCtmsNet& net) : TSignPredictor(net) {}
+	void build();
+	int predict(const TInt srcNId, const TInt desNId);
+};
+
+class TBalanceBasedPredictor : public TSignPredictor {
+public:
+	TBalanceBasedPredictor(const PCtmsNet& net) : TSignPredictor(net) {}
+	void build();
+	int predict(const TInt srcNId, const TInt desNId);
+};
+
+class TStatusBasedPredictor : public TSignPredictor {
+public:
+	TStatusBasedPredictor(const PCtmsNet& net) : TSignPredictor(net) {}
+	void build();
+	int predict(const TInt srcNId, const TInt desNId);
+};
+
+class TLogisticRegression : public TSignPredictor {
+protected:
+	THashSet<TChA> features; //TChA is feature string. Its corresponding Int ID is hash KeyId (int KeyId -> TChA Feature String)
+	void extractFeatures();
+public:
+	TLogisticRegression(const PCtmsNet& net) : TSignPredictor(net) {}
+	void build();
+	int predict(const TInt srcNId, const TInt desNId);
+};
+
+class TCTMSProbabilisticInference : public TSignPredictor {
+protected:
+	THashSet<TChA> features; //TChA is feature string. Its corresponding Int ID is hash KeyId (int KeyId -> TChA Feature String)
+	void extractFeatures();
+public:
+	TCTMSProbabilisticInference(const PCtmsNet& net) : TSignPredictor(net) {}
+	void build();
+	int predict(const TInt srcNId, const TInt desNId);
+};
+#endif // !SIGN_PREDICTION2_H
